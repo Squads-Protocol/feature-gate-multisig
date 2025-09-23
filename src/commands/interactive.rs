9fa37eff85beffe6ad1eddda4bfff148e31882db
0xRigel <orion@sqds.io>
2025-09-23 02:39:41 +0200

    chore: reordering

--- src/commands/interactive.rs
+++ src/commands/interactive.rs
@@ -1,5 +1,6 @@
 use crate::commands::{
-    approve_feature_gate_activation_proposal, config_command, create_command, execute_feature_gate_activation_proposal, show_command
+    approve_feature_gate_activation_proposal, config_command, create_command,
+    execute_feature_gate_activation_proposal, show_command,
 };
 use crate::squads::get_vault_pda;
 use crate::utils::*;
@@ -26,20 +27,17 @@ pub async fn interactive_mode() -> Result<()> {
                 let feepayer_path = prompt_for_fee_payer_path(&config)?;
                 create_command(&mut config, None, vec![], Some(feepayer_path)).await?;
             }
-            "Show feature gate multisig details" => {
-                let address = Text::new("Enter the main multisig address:").prompt()?;
-                show_command(&config, Some(address)).await?;
-            }
-            "Show configuration" => {
-                config_command(&config).await?;
-            }
             "Transaction Generation" => {
                 let feature_gate_multisig_address =
                     prompt_for_pubkey("Enter the feature gate multisig address:")?;
                 let feature_gate_id = get_vault_pda(&feature_gate_multisig_address, 0, None).0;
                 let fee_payer_path = prompt_for_fee_payer_path(&config)?;
 
-                let options = vec!["Approve feature gate activation proposal", "Exit"];
+                let options = vec![
+                    "Approve feature gate activation proposal",
+                    "Execute feature gate activation proposal",
+                    "Exit",
+                ];
                 let choice: &str = Select::new("What would you like to do?", options).prompt()?;
                 match choice {
                     "Approve feature gate activation proposal" => {
@@ -83,6 +81,13 @@ pub async fn interactive_mode() -> Result<()> {
                     "Exit" => break,
                     _ => unreachable!(),
                 }
+            }
+            "Show feature gate multisig details" => {
+                let address = Text::new("Enter the main multisig address:").prompt()?;
+                show_command(&config, Some(address)).await?;
+            }
+            "Show configuration" => {
+                config_command(&config).await?;
             }
             "Exit" => break,
             _ => unreachable!(),
